50c5a868d5a3bc78caa33b9f5c9a3c84c721e32b
MartinTAngell <marty@angellbros.com>
2024-01-02 01:10:46 +0000

    Installed more plugins

--- lua/marty/core/keymaps.lua
+++ lua/marty/core/keymaps.lua
@@ -36,3 +36,10 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Closes the current 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Switch to the next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Switch to the previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current file in a new tab" })
+
+-- Telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
