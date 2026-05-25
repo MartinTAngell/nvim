c3b86925758339fecf121fb3412219e85563e77e
Marty Angell <marty@angellbros.com>
2026-05-25 16:36:42 -0400

    Some files

---
+++ lua/config/core/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt
+
+-- Clipboard and mouse 
+opt.clipboard = "unnamedplus"
+opt.mouse = "a"
+
+-- Split 
+opt.splitright = true
+opt.splitbelow = true
+
+-- Remember past commands to allow undos
+-- to persist across sessions
+opt.undofile = true
+
+-- Prevent NVIM from saving annoying files
+opt.swapfile = false
+opt.backup = false
+
+-- Appearance
+opt.termguicolors = true
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes" 
+opt.scrolloff = 8
+opt.wrap = true
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Indentation
+opt.tabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.autoindent = true
+opt.smartindent = true
